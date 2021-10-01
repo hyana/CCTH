2417671b130bf46b87ccbdb87746144f675abcf3
Hyana Kang <hyana@hyanbug.local>
2021-10-01 14:36:41 -0400

    Create checkPermutation.py

---
+++ checkPermutation.py
@@ -0,0 +1,9 @@
+def checkPermutation(s, t):
+    if len(s) != len(t):
+        return False
+    s1 = sorted(s)
+    s2 = sorted(t)
+    for c1, c2 in zip(s1, s2) :
+        if c1 != c2:
+            return False
+    return True        
