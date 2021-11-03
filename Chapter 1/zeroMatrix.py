3c56a1573410829d9358680269b58ed92b3fbbef
Hyana Kang <hyana@hyanbug.local>
2021-11-02 20:50:21 -0400

    Finalized zeroMatrix.py

--- Chapter 1/zeroMatrix.py
+++ Chapter 1/zeroMatrix.py
@@ -1,11 +1,25 @@
-from _typeshed import FileDescriptor
-
-
 def zeroMatrix(matrix):
-    find zero first 
-    
-    then convert the matrices to zeroMatrix
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] == 0:
+                setNone(matrix, row, col)
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if matrix[row][col] == None:
+                matrix[row][col] = 0
+    return matrix
+
+# mark None instead of 0 so that replaced 0 is not counted as input 0
+def setNone(matrix, row, col):
+    for i in range(0,len(matrix[row])):
+        if matrix[row][i] != 0:
+            matrix[row][i] = None
+    for j in range(0, len(matrix)): 
+        if matrix[j][col] != 0:
+            matrix[j][col] = None
+    return matrix
 
 def main():
-    zeroMatrix()
+    zeroMatrix([[1,1,1],[1,0,1],[1,1,1],[1,1,0]])
 main()
